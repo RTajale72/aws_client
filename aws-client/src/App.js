6b7159381ca0f705bf29955dae3ebecf8668a0e9
Tajale72 <Tajale72@gmail.com>
2022-07-25 01:39:21 -0500

    updated react navba

--- aws-client/src/App.js
+++ aws-client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import SignUp from './components/SignUp/signup';
 
 function App() {
   return (
+    <>
    <div className="app">
     <Navbar />
    </div>
+   <SignUp /> 
+   </>
   );
 }
 
---
+++ aws-client/src/components/SignUp/signup.jsx
@@ -0,0 +1,16 @@
+
+
+function SignUp() {
+  
+        return(
+            <nav class="bg-dark navbar-dark navbar">
+            <div className="row col-12 d-flex justify-content-center text-white">
+                <h3>Registration</h3>
+            </div>
+        </nav>
+           
+        )
+    
+}
+
+export default SignUp
