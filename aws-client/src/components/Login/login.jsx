7770a1fbdc87f0880396f7de434331ff84d2796b
Tajale72 <Tajale72@gmail.com>
2022-07-26 17:33:48 -0500

    updated login and signup

--- aws-client/src/components/Login/login.jsx
+++ aws-client/src/components/Login/login.jsx
@@ -1,8 +1,9 @@
 import Navbar from "../Navbar/Navbar";
+import './login.css'
 
 function Login() {
     return (
-        <div>
+        <div className="login">
         <Navbar />  
         <h1>This is a login page</h1>
   </div>
--- aws-client/src/components/SignUp/signup.jsx
+++ aws-client/src/components/SignUp/signup.jsx
@@ -1,12 +1,17 @@
 import Navbar from "../Navbar/Navbar";
-
+//mport Registration from "../Registration/registration"
+import './signup.css';
 function SignUp() {
   
         return(
-                <div>
-                <Navbar />  
-                <h1>This is a Signup page</h1>
-          </div>
+                <div className="signup">
+                <Navbar /> 
+                <div className="register-title"><h1>Register</h1></div>
+                <div className="registration">
+                <h1>This is a signup page</h1>
+                </div> 
+               
+                </div>
         )
     
 }
