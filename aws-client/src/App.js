ba0b1ba8de790b4010b64a757fdc97fa3d4b82e9
Tajale72 <Tajale72@gmail.com>
2022-07-26 13:42:24 -0500

    updated index with routes

--- aws-client/src/App.js
+++ aws-client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-//import Login from './components/Login/login';
+// import Login from './components/Login/login';
 import Navbar from './components/Navbar/Navbar';
-//import SignUp from './components/SignUp/signup';
+// import SignUp from './components/SignUp/signup';
+// import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
